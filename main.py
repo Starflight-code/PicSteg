5c21748937844c398539a1eacc9f32de485a5e9e
Starflight-Code <sparkr123@gmail.com>
2024-12-13 20:09:28 -0600

    Revert "Started embed function", made by AI and unrelated to functionality This code was obviously generated by AI and does not relate to the process defined in our embedding documentation. This does not even have anything to do with embedding, or relate to the program it's in. It's useless.
    This reverts commit 50c1096742b9d27db2257476baa9a326b9846d4e.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # DO NOT USE AI FOR THIS, IT WILL NOT KNOW WHAT ANYTHING HERE IS
-import random
 
 from PIL import Image
 import string_serialize
@@ -18,22 +17,6 @@ if __name__ == '__main__':
     p = image.load()
     print(width, height)
 
-
-
-
-
-# Iterate over each pixel
-for y in range(height):
-    for x in range(width):
-        # Get the pixel value
-        r = random.randint(0, 100)
-        g = random.randint(0, 255)
-        b = random.randint(0, 120)
-
-        new_pixel = (r, g, b)  # Red color
-        image.putpixel((x, y), new_pixel)
-
-image.save("modified.png")
 """
 serialized = string_serialize.serialize("test string", 100)
 print(string_serialize.deserialize(serialized))
